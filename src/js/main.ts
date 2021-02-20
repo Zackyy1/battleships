2b3cb7936b6fa5d3d616a3d67e0246858dfc7f04
Zackyy1 <qwertyuk01@gmail.com>
2021-02-20 15:21:41 +0200

    info

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,30 @@
+/**
+ * 
+ * BATTLESHITS
+ *  plz "npm i"
+ * 
+ */
+
 module MyModule {
     export const Init = () => {
         console.log('Hello world!');
     }
+
+
+    
+     //     Make function(s) for each point
+     
+     // 1) Create battle grid (variable or static, doesnt matter)
+
+     // 2) Create system that allows certain ships on the grid that can be rotated
+
+     // 3) make one grid -> then make another, for enemy
+
+     // 4) when ships are assigned, begin battle (first player chooses where to strike. If misses - next player goes, if hit - go again)
+
+     
+
+
 }
 
 
